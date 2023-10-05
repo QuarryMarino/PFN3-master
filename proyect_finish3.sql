34c859fb3d4c36184ca139e20f411b628ef67a10
Quarry <quarryjmg@gmail.com>
2023-10-05 13:56:49 -0300

    ok

--- proyect_finish3.sql
+++ proyect_finish3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-10-2023 a las 19:21:41
+-- Tiempo de generación: 05-10-2023 a las 18:51:07
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,16 +24,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `clases`
+--
+
+CREATE TABLE `clases` (
+  `id` int(11) NOT NULL,
+  `id_teacher` int(11) DEFAULT NULL,
+  `students_list` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`students_list`)),
+  `name_clases` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clases`
+--
+
+INSERT INTO `clases` (`id`, `id_teacher`, `students_list`, `name_clases`) VALUES
+(1, 1, '[{\"id\": 1, \"nombre\": \"Estudiante Ana García\"}, {\"id\": 2, \"nombre\": \"Estudiante Luis Martínez\"}]', 'Matemáticas Avanzadas'),
+(2, 2, '[{\"id\": 3, \"nombre\": \"Estudiante Laura Sánchez\"}]', 'Ciencias de la Computación'),
+(3, 3, '[]', 'Historia del Arte');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `roll`
 --
 
 CREATE TABLE `roll` (
   `id` int(11) NOT NULL,
   `users_id` int(11) DEFAULT NULL,
-  `admin` varchar(250) DEFAULT NULL,
+  `roles` varchar(250) DEFAULT NULL,
   `teacher` varchar(250) DEFAULT NULL,
   `students` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roll`
+--
+
+INSERT INTO `roll` (`id`, `users_id`, `roles`, `teacher`, `students`) VALUES
+(1, 2, 'teacher', NULL, NULL),
+(2, 3, 'student', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -58,11 +88,22 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `apellido`, `email`, `password`, `Nacimiento`, `clases`, `roll_id`, `direccion`) VALUES
-(1, 'Quarry', 'Mariño', 'admi@admin', 'admin', '0.036082474226804123', NULL, 'admin', 'santo americo');
+(1, 'Quarry', 'Mariño', 'admi@admin', '$2y$10$yFJyjjFgGvAR.jnvBDpRj.kNQwpAFiKxx4OfhESMW9v1OEhYhKuQq', '1991-12-14', NULL, 'admin', 'santo americo'),
+(2, 'João', 'Silva', 'joao@example', 'joao@example', '1990-05-15', 'Yoga, Pilates', 'student', 'Rua Principal, 123'),
+(3, 'Mariaaaa', 'Santos', 'maria@example', '$2y$10$cwdXrRi2ixuSGpYB142fkeprDFTbrEQMuSefeThaXwCVZYxGFlxI6', '1985-09-20', 'Zumba, CrossFit', 'teacher', 'Avenida Secundária, 456'),
+(4, 'Pedro', 'Ferreira', 'pedro@example', 'pedro@example', '2000-03-10', 'Spinning, Musculação', 'student', 'Travessa da Rua, 789'),
+(5, 'test', 'test', 'test@test', 'test', '1114-12-14', '', 'student', 'test'),
+(6, 'tucacas', 'tucacas', 'tucaca@tu', 'tucacas', '1411-12-14', '', 'student', 'tucacas');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `clases`
+--
+ALTER TABLE `clases`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `roll`
@@ -82,16 +123,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `clases`
+--
+ALTER TABLE `clases`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `roll`
 --
 ALTER TABLE `roll`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
